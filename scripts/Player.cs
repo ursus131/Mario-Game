c34066a1e6ea86217964117ff291fee18d7f1942
ursus131 <artempodgorcuk10427@gmail.com>
2024-05-24 19:11:40 +0300

    Добавлены враги,анимация смерти,смена уровня, добавлено взаимодействие с врагами

---
+++ scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public PlayerSpriteRenderer smallRenderer;
+    public PlayerSpriteRenderer bigRenderer;
+
+    private DeathAnimation deathAnimation;
+
+    public bool big => bigRenderer.enabled;
+    public bool small => smallRenderer.enabled;
+    public bool dead => deathAnimation.enabled;
+
+    private void Awake()
+    {
+        deathAnimation = GetComponent<DeathAnimation>();
+    }
+    public void Hit()
+    {
+        if (big)
+        {
+            Shrink();
+        }
+        else
+        {
+            Death();
+        }
+    }
+
+    private void Shrink()
+    {
+        //
+    }
+
+    private void Death()
+    {
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = false;
+        deathAnimation.enabled = true;
+
+        GameManager.Instance.ResetLevel(3f);
+    }
+
+}
--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -98,7 +98,15 @@ public class PlayerMovement : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            if (transform.DotTest(collision.transform, Vector2.down))
+            {
+                velocity.y = jumpForce / 2f;
+                jumping = true;
+            }
+        }
+        else if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
         {
             if (transform.DotTest(collision.transform, Vector2.up))
             {
